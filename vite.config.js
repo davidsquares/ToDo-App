ff1b18cba9fc3005d99f3d55ff5e6d7656afb566
David Cuadros <daviddelaluz18@gmail.com>
2025-01-12 18:02:00 -0600

    commit final para chat

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: './O5-ToDo-App/', // Ajusta esto si tu app está en un subdirectorio
+    base: '/O5-ToDo-App/', // Ajusta esto si tu app está en un subdirectorio
 });
